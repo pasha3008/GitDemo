89461ec4443457f68d36778d9b7ceb626bb71a94
kshaik <kshaik.aws30@gmail.com>
2020-04-14 16:16:14 +0530

    Example3 file commited

---
+++ GitDemo/src/Example3.java
@@ -0,0 +1,11 @@
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("THis is Example3 of Java");
+		System.out.println("As per ur req I am adding one more check");
+
+	}
+
+}
