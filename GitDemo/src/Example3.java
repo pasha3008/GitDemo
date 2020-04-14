61cfba25fc61acfbeb5fb36b845331eb337669df
kshaik <kshaik.aws30@gmail.com>
2020-04-14 16:21:04 +0530

    commiting example3 from BranchZ

--- GitDemo/src/Example3.java
+++ GitDemo/src/Example3.java
@@ -5,6 +5,7 @@ public class Example3 {
 		// TODO Auto-generated method stub
 		System.out.println("THis is Example3 of Java");
 		System.out.println("As per ur req I am adding one more check");
+		System.out.println("I am checking out in branchz");
 
 	}
 
